635a3858b31473764566c0bb8f9e97864659347e
HammadNadeem21 <nhammad424@gmail.com>
2024-10-23 22:36:12 +0500

    updated

--- milestone3-dynamic-resume-builder/script.js
+++ milestone3-dynamic-resume-builder/script.js
@@ -1,25 +1,20 @@
-var submit = document.getElementById("submit");
-var inputName = document.getElementById("name");
-var outputName = document.getElementById("opName");
-var inputEmail = document.getElementById("email");
-var outputEmail = document.getElementById("opEmail");
-var inputPhone = document.getElementById("phone");
-var outputPhone = document.getElementById("opPhone");
-var inputEducation = document.getElementById("education");
-var outputEducation = document.getElementById("opEducation");
-var inputExperience = document.getElementById("experience");
-var outputExperience = document.getElementById("opExperience");
-var inputSkill = document.getElementById("skill");
-var outputSkill = document.getElementById("opSkill");
-var output = document.querySelector(".output");
-submit.addEventListener('click', function (event) {
+var form = document.getElementById('resume-form');
+var resumeDisplay = document.getElementById('resume-display');
+// Form Submition
+form.addEventListener('submit', function (event) {
     event.preventDefault();
-    outputName.innerHTML = inputName.value;
-    outputEmail.innerHTML = inputEmail.value;
-    outputPhone.innerHTML = inputPhone.value;
-    outputEducation.innerHTML = inputEducation.value;
-    outputExperience.innerHTML = inputExperience.value;
-    outputSkill.innerHTML = inputSkill.value;
-    output.style.display = "block";
-    output.scrollIntoView({ behavior: "smooth", block: "start" });
+    // Input Values
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+    var education = document.getElementById('education').value;
+    var experience = document.getElementById('experience').value;
+    var skills = document.getElementById('skills').value;
+    var resumeHtml = "\n    <h1>Resume</h1>\n    <h2>Personal Information</h2>\n    <p><b>Name:</b> ".concat(name, "</p>\n    <p><b>Email:</b> ").concat(email, "</p>\n    <p><b>Phone:</b> ").concat(phone, "</p>\n\n    <h2>Education</h2>\n    <p>").concat(education, "</p>\n\n    <h2>Experience</h2>\n    <p>").concat(experience, "</p>\n\n    <h2>Skills</h2>\n    <p>").concat(skills, "</p>\n\n    ");
+    if (resumeDisplay) {
+        resumeDisplay.innerHTML = resumeHtml;
+    }
+    else {
+        console.error("resume is missing");
+    }
 });
--- milestone3-dynamic-resume-builder/script.ts
+++ milestone3-dynamic-resume-builder/script.ts
@@ -1,36 +1,41 @@
-var submit = document.getElementById("submit") as HTMLButtonElement;
+var form = document.getElementById('resume-form') as HTMLFormElement;
+const resumeDisplay = document.getElementById('resume-display') as HTMLDivElement;
+
+// Form Submition
+form.addEventListener('submit', (event: Event) =>{
+    event.preventDefault();
 
 
-var inputName = document.getElementById("name") as HTMLInputElement;
-var outputName = document.getElementById("opName") as HTMLHeadingElement;
+    // Input Values
+    const name = (document.getElementById('name') as HTMLInputElement).value
+    const email = (document.getElementById('email') as HTMLInputElement).value
+    const phone = (document.getElementById('phone') as HTMLInputElement).value
+    const education = (document.getElementById('education') as HTMLInputElement).value
+    const experience = (document.getElementById('experience') as HTMLInputElement).value
+    const skills = (document.getElementById('skills') as HTMLInputElement).value
 
-var inputEmail = document.getElementById("email") as HTMLInputElement;
-var outputEmail = document.getElementById("opEmail") as HTMLHeadingElement;
+    const resumeHtml = `
+    <h1>Resume</h1>
+    <h2>Personal Information</h2>
+    <p><b>Name:</b> ${name}</p>
+    <p><b>Email:</b> ${email}</p>
+    <p><b>Phone:</b> ${phone}</p>
 
-var inputPhone = document.getElementById("phone") as HTMLInputElement;
-var outputPhone = document.getElementById("opPhone") as HTMLHeadingElement;
+    <h2>Education</h2>
+    <p>${education}</p>
 
-var inputEducation = document.getElementById("education") as HTMLInputElement;
-var outputEducation = document.getElementById("opEducation") as HTMLHeadingElement;
+    <h2>Experience</h2>
+    <p>${experience}</p>
 
-var inputExperience = document.getElementById("experience") as HTMLInputElement;
-var outputExperience = document.getElementById("opExperience") as HTMLHeadingElement;
+    <h2>Skills</h2>
+    <p>${skills}</p>
 
-var inputSkill = document.getElementById("skill") as HTMLInputElement;
-var outputSkill = document.getElementById("opSkill") as HTMLHeadingElement;
+    `;
 
-var output = document.querySelector(".output") as HTMLElement;
-
-submit.addEventListener('click', function(event){
-event.preventDefault();
-
-outputName.innerHTML = inputName.value;
-outputEmail.innerHTML = inputEmail.value;
-outputPhone.innerHTML = inputPhone.value
-outputEducation.innerHTML = inputEducation.value;
-outputExperience.innerHTML = inputExperience.value;
-outputSkill.innerHTML = inputSkill.value;
-output.style.display = "block";
-output.scrollIntoView({ behavior: "smooth", block:"start"})
-});
+    if(resumeDisplay){
+        resumeDisplay.innerHTML = resumeHtml;
+    }else{
+        console.error("resume is missing")
+    }
+})
 
